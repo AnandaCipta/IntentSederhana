db1ef8b4cbe92783e9628ff68ea8bda0fba11a19
AnandaCipta <ciptaananda67@gmal.com>
2020-09-01 20:12:22 +0700

    latihan intent

---
+++ app/src/main/java/com/example/intentsederhana/IntentActivity.java
@@ -0,0 +1,15 @@
+package com.example.intentsederhana;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // TODO Auto-generate method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biodata);
+    }
+
+}
